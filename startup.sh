0e07f3032a136d73f3ae8b35cbab93ac537b8b1c
Sam Larsen <samlarsen1@yahoo.co.uk>
2016-10-12 23:15:09 +0100

    added gitignore and updated startup

--- startup.sh
+++ startup.sh
@@ -8,17 +8,17 @@ echo
 echo "Installing docker"
 # Update apt sources
 echo " - updating apt sources"
-sudo apt-get install -y apt-transport-https ca-certificates
+sudo apt-get install -y apt-transport-https ca-certificates  > /dev/null
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' > /etc/apt/sources.list.d/docker.list
 sudo apt-get update  > /dev/null
-sudo apt-get purge lxc-docker
+sudo apt-get purge lxc-docker  > /dev/null
 # Install extra kernel packages
 echo " - installing extra kernel packages"
-sudo apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual  > /dev/null
 # Install docker
 echo " - installing docker"
-sudo apt-get update   > /dev/null
+sudo apt-get update  > /dev/null
 sudo apt-get install docker-engine  > /dev/null
 echo " - starting docker daemon"
 sudo service docker start
